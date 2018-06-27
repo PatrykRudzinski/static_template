603ad9263081c4ac31b41c8da977e57de8ea2fa6
patryk <patrykrudzinski@gmail.com>
2018-06-27 22:50:43 +0200

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,56 @@
-'use strict'
+'use strict';
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const plumber = require('gulp-plumber');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
 const notify = require('gulp-notify');
+const wait = require('gulp-wait')
 
 function showError(err) {
-
     notify.onError({
         title: "Gulp error in " + err.plugin,
-        message:  err.message
+        message: err.message
     })(err);
+
     this.emit('end');
 }
 
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
+
+//---------------------------------------------------------------------------------- Sass
+gulp.task('sass', function() {
+    return gulp.src('./scss/main.scss')
+        .pipe(wait(100))
+        .pipe(plumber({errorHandler : showError}))
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(autoprefixer({browsers: ['last 2 versions']}))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.reload({
+        stream: true
+    }))
+});
+
+//---------------------------------------------------------------------------------- Watch
+gulp.task('watch', function(){
+    gulp.watch('./scss/**/*.scss', ['sass']);
+    gulp.watch('./*.html', browserSync.reload);
+    gulp.watch('js/**/*.js', browserSync.reload);
+});
+
+//---------------------------------------------------------------------------------- BrowserSync
+gulp.task('browserSync', function(){
+   browserSync.init({
+       server: {
             baseDir: './'
         },
         notify: false
-        //host: 'IP numer'
-        //port: 3000
-        //browser: 'google chrome'/'chrome'
-    });
-});
-
-gulp.task('sass', function () {
-    return gulp.src('./scss/main.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest('./css'))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('watch', function () {
-    gulp.watch('./scss/**/*.scss', ['sass']);
-    gulp.watch("./**/*.js", browserSync.reload());
-    gulp.watch('./*.html').on('change', browserSync.reload);
+   })
 });
 
 gulp.task('default', function () {
-    gulp.start(['sass', 'browser-sync', 'watch']);
+     gulp.start(['sass', 'browserSync', 'watch']);
 });
-
